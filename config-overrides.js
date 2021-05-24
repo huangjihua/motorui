7ff1eae9a6ae1861ed363a84e36f453b7b79f426
hank <steven-online@hotmail.com>
2021-05-24 19:31:37 +0800

    build: update config

--- config-overrides.js
+++ config-overrides.js
@@ -6,9 +6,13 @@ const {
   addBabelPlugins,
 } = require('customize-cra');
 const path = require('path');
-const WebpackSpritesmithPlugin = require('webpack-spritesmith');
-const TerserPlugin = require('terser-webpack-plugin');
-
+// const WebpackSpritesmithPlugin = require('webpack-spritesmith');
+// const TerserPlugin = require('terser-webpack-plugin');
+// var commonsPlugin = new webpack.optimize.CommonsChunkPlugin(/* chunkName= */'common', /* filename= */'common.js'); // 分析以下模块的共用代码, 单独打一个包到common.js
+// var ExtractTextPlugin = require("extract-text-webpack-plugin"); // 单独打包CSS
+// var HtmlWebpackPlugin = require('html-webpack-plugin'); // Html文件处理
+// 接收运行参数
+const argv = require('yargs').describe('debug', 'debug 环境').argv; // use 'webpack --debug'
 const rewiredMap = () => (config) => {
   config.devtool = config.mode === 'development' ? 'source-map' : false;
   return config;
@@ -22,6 +26,6 @@ module.exports = override(
     '@': path.resolve(__dirname, './src'),
     '@comps': path.resolve(__dirname, './src/components'),
   }),
-  addWebpackPlugin(new vConsolePlugin()),
+  addWebpackPlugin(new vConsolePlugin({ enable: !!argv.debug })),
   rewiredMap()
 );
